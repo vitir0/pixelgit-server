87d198e4b56ad6330604d81b263f8613c7bb86f3
vitir._ <140893548+vitir0@users.noreply.github.com>
2025-06-21 18:45:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,19 @@ from psycopg2.extras import RealDictCursor
 app = Flask(__name__)
 CORS(app)
 
+# Добавляем обработчики CORS
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        return jsonify({"status": "ok"}), 200
+
 # Конфигурация
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'pixelgit_secret_2025')
 
